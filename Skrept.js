9d6843cbd2bbbe28f5c2b626e5b030ca25eae1bc
Ivanka_Da <sepilovivan89@gmail.com>
2026-05-10 04:12:12 +0700

    задача

--- Skrept.js
+++ Skrept.js
@@ -1,16 +1,10 @@
-var privet = 9
 
-if(privet < 10){
-	console.log("crabotalo")
-}else{
-	console.log("nesrabotalo")
+function checkwizard(x,y,z){
+	return x >= 65 || y >= 40 || z == true
 }
 
-var poka = privet < 8 || privet > 10
 
-
-
-
-
-
-console.log(poka)
+console.log(checkwizard(10,20,false))
+console.log(checkwizard(65,40,true))
+console.log(checkwizard(100,100,false))
+console.log(checkwizard(10,50,false))
