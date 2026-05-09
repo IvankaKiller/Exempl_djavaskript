90af26a696193cb2f05c9a89bfa156a06f76646f
Ivanka_Da <sepilovivan89@gmail.com>
2026-05-10 04:23:22 +0700

    master alert

--- Skrept.js
+++ Skrept.js
@@ -1,10 +1,3 @@
-
-function checkwizard(x,y,z){
-	return x >= 65 || y >= 40 || z == true
-}
-
-
-console.log(checkwizard(10,20,false))
-console.log(checkwizard(65,40,true))
-console.log(checkwizard(100,100,false))
-console.log(checkwizard(10,50,false))
+var prove = Math.floor(3.5)
+console.log(prove)
+alert("kaka, popa")
